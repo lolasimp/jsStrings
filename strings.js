978dc0b0f5e3db14f2a137620c23f9eaacabc8d1
Lola Simpson <lolalsimp@gmail.com>
2018-02-26 20:42:20 -0600

    added challenge 3

--- strings.js
+++ strings.js
@@ -3,6 +3,16 @@
 var numOfSandwhiches = 4 * 2 ;
 console.log("numOfSandwhiches", numOfSandwhiches);
 
+//Challenge 2
 var name = "Lola";
+
 var string= "Hello, " + name + " how are you today?";
-console.log(“string”, string); 
+
+console.log(“string:”, string); 
+
+//Challenge #3
+var DNA = “GCAT”;
+
+var RNA = DNA.replace('T', 'U');
+
+console.log(“RNA”, RNA);
