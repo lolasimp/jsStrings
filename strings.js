01ddc147f89a85a3c58f607bd524c5a280012091
Lola Simpson <lolalsimp@gmail.com>
2018-02-26 22:00:03 -0600

    added html and js

--- strings.js
+++ strings.js
@@ -6,23 +6,25 @@ console.log("numOfSandwhiches", numOfSandwhiches);
 //Challenge 2
 var name = "Lola";
 
-var string= "Hello, " + name + " how are you today?";
+var string = "Hello, " + name + " how are you today?";
 
-console.log(“string:”, string); 
+console.log("string", string); 
 
 //Challenge #3
-var DNA = “GCAT”;
+var DNA = "GCAT";
 
 var RNA = DNA.replace('T', 'U');
 
-console.log(“RNA”, RNA);
+console.log("RNA", RNA);
 
 // Challenge #4
 
 var animal = 'Goat'; 
 var animal = 'alligator';
-if(animal === 'alligator'){ //animal.toLowerCase()=== 'alligator'
-        console.log("small");
+if(animal === 'alligator'){
+    // debugger; 
+    animal.toLowerCase()=== 'alligator'
+        // console.log("small");
 }else{
      console.log("wide");
 }
@@ -32,3 +34,10 @@ if(animal === 'Goat'){
 }else{
     console.log('wide');
 }
+
+
+var yarn = "the better string";
+var domString = "<h4>" + yarn + "<h4>";
+
+var myDiv =document.getElementById('yarn-holder');
+myDiv.innerHTML = domString;
